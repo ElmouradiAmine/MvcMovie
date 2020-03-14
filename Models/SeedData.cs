9bf4a11b7d40b2a6933c1a04c1e00e2f28cdca1a
Amine EL MOURADI <elmouradi.amine98@gmail.com>
2020-03-14 19:46:07 +0100

    Adding rating property to a movie.

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -25,28 +25,32 @@ namespace MvcMovie.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
-                        Price = 7.99M
+                        Price = 7.99M,
+                        Rating = "R"
                     },
                     new Movie
                     {
                         Title = "Ghostbusters",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Comedy",
-                        Price = 8.99M
+                        Price = 8.99M,
+                        Rating = "R"
                     },
                     new Movie
                     {
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Comedy",
-                        Price = 9.99M
+                        Price = 9.99M,
+                        Rating = "R"
                     },
                     new Movie
                     {
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Western",
-                        Price = 3.99M
+                        Price = 3.99M,
+                        Rating = "R"
                     }
 
                     ) ;
